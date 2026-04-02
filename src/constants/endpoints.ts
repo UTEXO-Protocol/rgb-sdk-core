d6c42808c1bd336440670eb27c50fde46337af2a
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2026-04-02 13:54:04 +0300

    feat: add utexo config

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,7 +4,8 @@ export const DEFAULT_TRANSPORT_ENDPOINTS: Record<Network, string> = {
   mainnet: 'rpcs://rgb-proxy-mainnet.utexo.com/json-rpc',
   testnet: 'rpcs://rgb-proxy-testnet3.utexo.com/json-rpc',
   testnet4: 'rpcs://proxy.iriswallet.com/0.2/json-rpc',
-  signet: 'rpcs://rgb-proxy-utexo.utexo.com/json-rpc',
+  signet: 'rpcs://proxy.iriswallet.com/0.2/json-rpc',
+  utexo: 'rpcs://rgb-proxy-utexo.utexo.com/json-rpc',
   regtest: 'rpcs://proxy.iriswallet.com/0.2/json-rpc',
 };
 
@@ -12,6 +13,7 @@ export const DEFAULT_INDEXER_URLS: Record<Network, string> = {
   mainnet: 'ssl://electrum.iriswallet.com:50003',
   testnet: 'ssl://electrum.iriswallet.com:50013',
   testnet4: 'ssl://electrum.iriswallet.com:50053',
-  signet: 'https://esplora-api.utexo.com',
+  signet: 'ssl://electrum.iriswallet.com:50033',
+  utexo: 'https://esplora-api.utexo.com',
   regtest: 'tcp://regtest.thunderstack.org:50001',
 };
